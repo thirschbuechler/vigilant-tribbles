953cf2e5e369b375424ec5a30679124b339f0de6
thirschbuechler <hirschbuechler@ift.at>
2022-03-11 18:06:42 +0100

    mailüfterl init - autofloor, autoround

---
+++ mailuefterl.py
@@ -0,0 +1,50 @@
+# some general computation bla
+
+import numpy as np
+
+def auto_round(x):
+    """
+    round to significance
+
+    >>> auto_round(15000)
+    15000
+    >>> auto_round(15555)
+    16000
+    >>> auto_round(153)
+    150
+    """
+    r = - int (np.log10(x)) + 1 # neg to round left of comma
+    return (round(x, r))
+
+
+def auto_floor(x):
+    """
+    round to significance
+
+    >>> auto_floor(15000)
+    15000
+    >>> auto_floor(15555)
+    15000
+    >>> auto_floor(153)
+    150
+    """
+    r =  int (np.log10(x)) - 1 # neg to round left of comma
+    expo = np.power(10,r)
+    #print(x/expo)
+    x = np.floor(x/expo)*expo
+    return int(x)
+
+
+def integritycheck():
+    """ better call doctest """
+    import doctest
+    print("performing doctest test..")
+    res=doctest.testmod() # process doctest methods
+    print(res)
+    print("attempted==succeeded, if no fails\n")
+
+
+
+#-#-# module test #-#-#
+if __name__ == '__main__': # test if called as executable, not as library
+    integritycheck()#does not work f class functions?
