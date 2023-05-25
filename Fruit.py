1e6204720e9ea49daaec234c20ddbc4a145ecac1
thirschbuechler <thirschbuechler@github.com>
2023-05-25 10:33:31 +0200

    outsource logger to Fruit

--- Fruit.py
+++ Fruit.py
@@ -28,13 +28,50 @@ Created 230523
 
 @author: thirschbuechler
 """
+import logging
+
 class Fruit(object):
     #__metaclass__ = GetAttr
     def __init__(self, ID=None, root=None, 
                     data=[], data_x=[], metadata={},
                     **kwargs):
+        
         super().__init__(**kwargs) # (*args, **kwargs) # superclass inits
         
+        # # logger setup
+        levels_now = logging._levelToName.values()
+        addLoggingLevel('TRAIL',  logging.INFO - 2)
+        addLoggingLevel('CRUMB', logging.DEBUG + 2)
+        """Levels
+        CRITICAL
+        ERROR
+        WARNING
+        INFO
+        *Trail
+        *Crumb
+        DEBUG # spammed by matplotlib
+        NOTSET
+        """
+        basiclogger = True
+        if basiclogger:
+            #logging.basicConfig(level=logging.INFO)
+            logging.basicConfig(level=logging.TRAIL)
+            # then logging.info() etc works
+            self.log = logging
+        else:    # doesn't work
+            self.log = logging.getLogger(__class__.__name__)
+            # create console handler and set level to debug
+            ch = logging.StreamHandler()
+            #ch.setLevel(logging.INFO)
+            ch.setLevel(logging.TRAIL)
+            # create formatter
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            # add formatter to ch
+            ch.setFormatter(formatter)
+            # add ch to logger
+            self.log.addHandler(ch)
+
+        # # class props etc setup
         self.root = root # root obj
         self.ID = ID # name of this container
 
@@ -49,6 +86,7 @@ class Fruit(object):
         self.data = data
         self.data_x = data_x
         
+
     def __getitem__(self, obj): # Attribute handler
         return self.__dict__[obj]
     
@@ -101,6 +139,61 @@ class Fruit(object):
         meta.update(self.metadata)
         return meta
 
+
+def addLoggingLevel(levelName, levelNum, methodName=None):
+    """
+    Comprehensively adds a new logging level to the `logging` module and the
+    currently configured logging class.
+
+    `levelName` becomes an attribute of the `logging` module with the value
+    `levelNum`. `methodName` becomes a convenience method for both `logging`
+    itself and the class returned by `logging.getLoggerClass()` (usually just
+    `logging.Logger`). If `methodName` is not specified, `levelName.lower()` is
+    used.
+
+    To avoid accidental clobberings of existing attributes, this method will
+    raise an `AttributeError` if the level name is already an attribute of the
+    `logging` module or if the method name is already present 
+
+    Example
+    -------
+    >>> addLoggingLevel('TRACE', logging.DEBUG - 5)
+    >>> logging.getLogger(__name__).setLevel("TRACE")
+    >>> logging.getLogger(__name__).trace('that worked')
+    >>> logging.trace('so did this')
+    >>> logging.TRACE
+    5
+
+    
+    https://stackoverflow.com/questions/2183233/how-to-add-a-custom-loglevel-to-pythons-logging-facility/35804945#35804945
+    """
+    fine=True
+    while(fine):
+        if not methodName:
+            methodName = levelName.lower()
+
+        if hasattr(logging, levelName):
+            #raise AttributeError('{} already defined in logging module'.format(levelName))
+            break
+        if hasattr(logging, methodName):
+            raise AttributeError('{} already defined in logging module'.format(methodName))
+        if hasattr(logging.getLoggerClass(), methodName):
+            raise AttributeError('{} already defined in logger class'.format(methodName))
+
+        # This method was inspired by the answers to Stack Overflow post
+        # http://stackoverflow.com/q/2183233/2988730, especially
+        # http://stackoverflow.com/a/13638084/2988730
+        def logForLevel(self, message, *args, **kwargs):
+            if self.isEnabledFor(levelNum):
+                self._log(levelNum, message, args, **kwargs)
+        def logToRoot(message, *args, **kwargs):
+            logging.log(levelNum, message, *args, **kwargs)
+
+        logging.addLevelName(levelNum, levelName)
+        setattr(logging, levelName, levelNum)
+        setattr(logging.getLoggerClass(), methodName, logForLevel)
+        setattr(logging, methodName, logToRoot)
+        fine=False
 
 
 def fruittester():
