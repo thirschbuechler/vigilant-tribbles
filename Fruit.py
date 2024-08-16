1926963b66dbce2a278253a9a5d70e1a7af3f004
thirschbuechler <thirschbuechler@github.com>
2024-08-16 10:34:43 +0200

    Fruit - add custom loglevel before setting it as default in init

--- Fruit.py
+++ Fruit.py
@@ -42,6 +42,68 @@ from vigilant_tribbles.mailuefterl import bin_to_xaxis
 sys.path.pop(len(sys.path)-1)
 
 
+# # add custom loglevels
+def addLoggingLevel(levelName, levelNum, methodName=None):
+    """
+    Comprehensively adds a new logging level to the `logging` module and the
+    currently configured logging class.
+
+    `levelName` becomes an attribute of the `logging` module with the value
+    `levelNum`. `methodName` becomes a convenience method for both `logging`
+    itself and the class returned by `logging.getLoggerClass()` (usually just
+    `logging.Logger`). If `methodName` is not specified, `levelName.lower()` is
+    used.
+
+    To avoid accidental clobberings of existing attributes, this method will
+    raise an `AttributeError` if the level name is already an attribute of the
+    `logging` module or if the method name is already present 
+
+    Example
+    -------
+    >>> addLoggingLevel('TRACE', logging.DEBUG - 5)
+    >>> logging.getLogger(__name__).setLevel("TRACE")
+    >>> logging.getLogger(__name__).trace('that worked')
+    >>> logging.trace('so did this')
+    >>> logging.TRACE
+    5
+
+    
+    https://stackoverflow.com/questions/2183233/how-to-add-a-custom-loglevel-to-pythons-logging-facility/35804945#35804945
+    """
+    fine=True
+    while(fine):
+        if not methodName:
+            methodName = levelName.lower()
+
+        if hasattr(logging, levelName):
+            #raise AttributeError('{} already defined in logging module'.format(levelName))
+            break
+        if hasattr(logging, methodName):
+            raise AttributeError('{} already defined in logging module'.format(methodName))
+        if hasattr(logging.getLoggerClass(), methodName):
+            raise AttributeError('{} already defined in logger class'.format(methodName))
+
+        # This method was inspired by the answers to Stack Overflow post
+        # http://stackoverflow.com/q/2183233/2988730, especially
+        # http://stackoverflow.com/a/13638084/2988730
+        def logForLevel(self, message, *args, **kwargs):
+            if self.isEnabledFor(levelNum):
+                self._log(levelNum, message, args, **kwargs)
+        def logToRoot(message, *args, **kwargs):
+            logging.log(levelNum, message, *args, **kwargs)
+
+        logging.addLevelName(levelNum, levelName)
+        setattr(logging, levelName, levelNum)
+        setattr(logging.getLoggerClass(), methodName, logForLevel)
+        setattr(logging, methodName, logToRoot)
+        fine=False
+
+    
+addLoggingLevel('TRAIL',  logging.INFO - 2)
+addLoggingLevel('CRUMB', logging.DEBUG + 2)
+# levels_now = logging._levelToName.values()
+# docu see: def self.setLogLevel()
+
 
 class Fruit(object):
     #__metaclass__ = GetAttr
@@ -60,13 +122,6 @@ class Fruit(object):
         super().__init__(**kwargs) # (*args, **kwargs) # superclass inits
         
         # # logger setup
-        
-        # add custom loglevels
-        addLoggingLevel('TRAIL',  logging.INFO - 2)
-        addLoggingLevel('CRUMB', logging.DEBUG + 2)
-        # levels_now = logging._levelToName.values()
-        # docu see: def self.setLogLevel()
-
         # basicconfig - ok for small projects
         if basiclogger:
             logging.basicConfig(level=loglevel)
@@ -218,62 +273,6 @@ class Fruit(object):
             meta = {}
         meta.update(self.metadata)
         return meta
-
-
-def addLoggingLevel(levelName, levelNum, methodName=None):
-    """
-    Comprehensively adds a new logging level to the `logging` module and the
-    currently configured logging class.
-
-    `levelName` becomes an attribute of the `logging` module with the value
-    `levelNum`. `methodName` becomes a convenience method for both `logging`
-    itself and the class returned by `logging.getLoggerClass()` (usually just
-    `logging.Logger`). If `methodName` is not specified, `levelName.lower()` is
-    used.
-
-    To avoid accidental clobberings of existing attributes, this method will
-    raise an `AttributeError` if the level name is already an attribute of the
-    `logging` module or if the method name is already present 
-
-    Example
-    -------
-    >>> addLoggingLevel('TRACE', logging.DEBUG - 5)
-    >>> logging.getLogger(__name__).setLevel("TRACE")
-    >>> logging.getLogger(__name__).trace('that worked')
-    >>> logging.trace('so did this')
-    >>> logging.TRACE
-    5
-
-    
-    https://stackoverflow.com/questions/2183233/how-to-add-a-custom-loglevel-to-pythons-logging-facility/35804945#35804945
-    """
-    fine=True
-    while(fine):
-        if not methodName:
-            methodName = levelName.lower()
-
-        if hasattr(logging, levelName):
-            #raise AttributeError('{} already defined in logging module'.format(levelName))
-            break
-        if hasattr(logging, methodName):
-            raise AttributeError('{} already defined in logging module'.format(methodName))
-        if hasattr(logging.getLoggerClass(), methodName):
-            raise AttributeError('{} already defined in logger class'.format(methodName))
-
-        # This method was inspired by the answers to Stack Overflow post
-        # http://stackoverflow.com/q/2183233/2988730, especially
-        # http://stackoverflow.com/a/13638084/2988730
-        def logForLevel(self, message, *args, **kwargs):
-            if self.isEnabledFor(levelNum):
-                self._log(levelNum, message, args, **kwargs)
-        def logToRoot(message, *args, **kwargs):
-            logging.log(levelNum, message, *args, **kwargs)
-
-        logging.addLevelName(levelNum, levelName)
-        setattr(logging, levelName, levelNum)
-        setattr(logging.getLoggerClass(), methodName, logForLevel)
-        setattr(logging, methodName, logToRoot)
-        fine=False
 
 
 def fruittester():
